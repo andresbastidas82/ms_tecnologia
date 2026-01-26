0fef58fffbb0f7fdcf318189011ed902595ab00d
Wilson Bastidas <wilson.bastidas@pragma.com.co>
2026-01-26 13:22:32 -0500

    initial commit

---
+++ src/test/java/com/pragma/ms_tecnologia/MsTecnologiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.ms_tecnologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTecnologiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
